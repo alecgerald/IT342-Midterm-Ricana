1d9e7ca52e2f1c6edf0a277ddbc05ba835cca8c6
alecgerald <alecgerald.ricana@gmail.com>
2025-03-08 18:28:06 +0800

    Google OAuth added

---
+++ activity-midterm/activity-midterm/src/main/java/com/ricana/activity_midterm/ActivityMidtermApplication.java
@@ -0,0 +1,13 @@
+package com.ricana.activity_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityMidtermApplication.class, args);
+	}
+
+}
---
+++ activity-midterm/activity-midterm/src/test/java/com/ricana/activity_midterm/ActivityMidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricana.activity_midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityMidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
